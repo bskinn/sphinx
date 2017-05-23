0c4da150c124f9a43baf05554e925bfa51f10a37
Brian Skinn <brian.skinn@gmail.com>
2017-05-23 17:49:45 -0400

    DEV: Mainly %SILENT%, %ANYFAILED%
    make.bat.new_t:
    
     * Change from %0 to explicit make when making the recursive calls.
       Was some path weirdness causing these calls to fail.
     * Fix %ERRORLEVEL% check by changing to numeric test
    
    test_winmake.bat:
    
     * Add %SILENT% and handling of '-s' flag; roll out output
       suppression if '-s' supplied
     * Add %ANYFAILED%, as 'sticky' flag for failure at any point in
       the series of tests; add exit based on %ANYFAILED% value
     * Exit with error if both '-v' and '-s' are supplied
     * Add tests:
        * make latex
        * make clean html
        * make html -a
        * make html -E

--- tests/winmake/test_winmake.bat
+++ tests/winmake/test_winmake.bat
@@ -5,7 +5,9 @@ setlocal
 rem Set defaults
 set KEEP=0
 set VERBOSE=0
+set SILENT=0
 set TESTDIR=testdir
+set ANYFAILED=0
 
 set HDR1======
 set HDR2=-----
@@ -26,6 +28,12 @@ if "%1"=="-v" (
     goto arg_loop
     )
 
+if "%1"=="-s" (
+    set SILENT=1
+    shift /1
+    goto arg_loop
+    )
+
 if "%1"=="-h" (
     echo.
     echo Test runner for Windows make.bat
@@ -35,6 +43,7 @@ if "%1"=="-h" (
     echo.
     echo -h   - Show this help
     echo -k   - Keep test directory after running tests
+    echo -s   - Run tests with all output suppressed
     echo -v   - Show output from executed commands
     echo.
     goto end_help
@@ -43,10 +52,22 @@ if "%1"=="-h" (
 
 :end_arg_loop
 
+rem Can't have both silent and verbose
+if %SILENT% EQU 1 (
+    if %VERBOSE% EQU 1 (
+        echo Cannot specify both '-s' and '-v'!
+        echo Exiting ...
+        set ANYFAILED=1
+        goto end_help
+        )
+    )
+
 
 rem Quickstart Sphinx into new directory
-echo %HDR1% Creating new Sphinx doc sandbox ...
-echo.
+if %SILENT% EQU 0 (
+    echo %HDR1% Creating new Sphinx doc sandbox ...
+    echo.
+    )
 
 if exist %TESTDIR%\nul rmdir /s /q %TESTDIR%
 
@@ -57,17 +78,21 @@ if "%VERBOSE%"=="1" (
     )
 
 if %ERRORLEVEL% GTR 0 (
-    echo.
-    echo %HDR1% Error quick-starting Sphinx.
-    echo %HDR1% Exiting...
-    echo.
+    if %SILENT% EQU 0 (
+        echo.
+        echo %HDR1% Error quick-starting Sphinx.
+        echo %HDR1% Exiting...
+        echo.
+        )
     goto end
     )
 
 
 rem Run the EXPECT-SUCCESS tests
-echo %HDR1% Running make operations expected to succeed:
-echo.
+if %SILENT% EQU 0 (
+    echo %HDR1% Running make operations expected to succeed:
+    echo.
+    )
 set ELVAL=0
 
 rem === make -h ===
@@ -91,11 +116,41 @@ goto run_test
 :RET_HTML
 set TEST_HTML=%RESULT%
 
+rem === make latex (no regen of env) ===
+set PARAMS=latex
+set RETURN=RET_LATEX
+goto run_test
+:RET_LATEX
+set TEST_LATEX=%RESULT%
+
+rem === make clean html ===
+set PARAMS=clean html
+set RETURN=RET_CLEAN_HTML
+goto run_test
+:RET_CLEAN_HTML
+set TEST_CLEAN_HTML=%RESULT%
+
+rem === make html -a ===
+set PARAMS=html -a
+set RETURN=RET_HTML_A
+goto run_test
+:RET_HTML_A
+set TEST_HTML_A=%RESULT%
+
+rem === make html -E ===
+set PARAMS=html -E
+set RETURN=RET_HTML_E
+goto run_test
+:RET_HTML_E
+set TEST_HTML_E=%RESULT%
+
 
 rem Run the EXPECT-FAIL tests
-echo.
-echo %HDR1% Running make operations expected to fail:
-echo.
+if %SILENT% EQU 0 (
+    echo.
+    echo %HDR1% Running make operations expected to fail:
+    echo.
+    )
 set ELVAL=1
 
 rem === make -b html ===
@@ -122,25 +177,31 @@ set TEST_FOO=%RESULT%
 
 
 rem Report Results
-echo.
-echo.
-echo ==========================================
-echo                TEST RESULTS
-echo ==========================================
-echo.
-echo          Args           Expect    Result
-echo  --------------------  --------  --------
-echo   -h                      OK      %TEST_H%
-echo   clean                   OK      %TEST_CLEAN%
-echo   html                    OK      %TEST_HTML%
-echo.
-echo   -b html                ERROR    %TEST_B_HTML%
-echo   clean html -c ..       ERROR    %TEST_CLEAN_HTML_C_dd%
-echo   foo                    ERROR    %TEST_FOO%
-echo.
-echo ==========================================
-echo.
-echo.
+if %SILENT% EQU 0 (
+    echo.
+    echo.
+    echo ==========================================
+    echo                TEST RESULTS
+    echo ==========================================
+    echo.
+    echo          Args           Expect    Result
+    echo  --------------------  --------  --------
+    echo   -h                      OK      %TEST_H%
+    echo   clean                   OK      %TEST_CLEAN%
+    echo   html                    OK      %TEST_HTML%
+    echo   latex                   OK      %TEST_LATEX%
+    echo   clean html              OK      %TEST_CLEAN_HTML%
+    echo   html -a                 OK      %TEST_HTML_A%
+    echo   html -E                 OK      %TEST_HTML_E%
+    echo.
+    echo   -b html                ERROR    %TEST_B_HTML%
+    echo   clean html -c ..       ERROR    %TEST_CLEAN_HTML_C_dd%
+    echo   foo                    ERROR    %TEST_FOO%
+    echo.
+    echo ==========================================
+    echo.
+    echo.
+    )
 
 goto end
 
@@ -149,7 +210,7 @@ goto end
 rem Run the test, with output suppressed by default
 if "%VERBOSE%"=="1" echo. & echo.
 
-echo %HDR2% Testing 'make %PARAMS%' ...
+if %SILENT% EQU 0 (echo %HDR2% Testing 'make %PARAMS%' ...)
 
 if "%VERBOSE%"=="0" (
     call %TESTDIR%\make %PARAMS% 1>nul 2>nul
@@ -162,6 +223,7 @@ if %ERRORLEVEL% EQU %ELVAL% (
     set RESULT=-pass-
     ) else (
     set RESULT=*FAIL*
+    set ANYFAILED=1
     )
 
 rem "Callback" to the particular test
@@ -175,17 +237,25 @@ goto %RETURN%
 
 rem Remove test folder if it exists and KEEP is 0
 if "%KEEP%"=="0" (
-    echo %HDR1% Removing Sphinx doc sandbox ...
-    echo.
+    if %SILENT% EQU 0 (
+        echo %HDR1% Removing Sphinx doc sandbox ...
+        echo.
+        )
     if exist %TESTDIR%\nul rmdir /s /q %TESTDIR%
-    echo %HDR1% Done.
-    echo.
+    if %SILENT% EQU 0 (
+        echo %HDR1% Done.
+        echo.
+        )
     ) else (
-    echo %HDR1% Sphinx doc sandbox left in place.
-    echo.
+    if %SILENT% EQU 0 (
+        echo %HDR1% Sphinx doc sandbox left in place.
+        echo.
+        )
     )
 
 :end_help
 
-endlocal
+endlocal & set ANYFAILED=%ANYFAILED%
+
+exit /B %ANYFAILED%
 
